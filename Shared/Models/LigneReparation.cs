38b6b8463f3268d21240166be326c716694c5ef1
axel_lefaux <Kaizen59840@gmail.com>
2017-04-27 11:11:31 +0200

    Completion du modele Reparation et creation du modele LigneReparation

---
+++ Shared/Models/LigneReparation.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.Models
+{
+    class LigneReparation
+    {
+        public int IdLigneReparation { get; set; }
+        public string Nom { get; set; }
+        public decimal PrixUnite { get; set; }
+        public int Unites { get; set; }
+        public decimal PrixTotal { get; set; }
+    }
+}
--- Shared/Models/Reparation.cs
+++ Shared/Models/Reparation.cs
@@ -8,5 +8,9 @@ namespace Shared.Models
 {
     class Reparation
     {
+        public int IdReparation { get; set; }
+        public DateTime Date { get; set; }
+        public decimal Prix { get; set; }
+        public string Lieu { get; set; }
     }
 }
